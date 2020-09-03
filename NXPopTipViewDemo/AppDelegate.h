1d46968c724e5cd7a30e84948b3efbc38990977f
angel <zx57885161@outlook.com>
2020-09-03 08:44:21 +0800

    Initial Commit

---
+++ NXPopTipViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NXPopTipViewDemo
+//
+//  Created by next on 2020/9/3.
+//  Copyright © 2020 next. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
